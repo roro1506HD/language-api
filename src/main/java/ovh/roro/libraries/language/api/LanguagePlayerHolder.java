d716850dae807e009d8554531c0dcb9685c0f9f0
roro1506HD <roro1506mcpro@gmail.com>
2023-10-07 15:41:13 +0200

    Removed 'OnlyOverride' annotation

--- src/main/java/ovh/roro/libraries/language/api/LanguagePlayerHolder.java
+++ src/main/java/ovh/roro/libraries/language/api/LanguagePlayerHolder.java
@@ -1,9 +1,7 @@
 package ovh.roro.libraries.language.api;
 
-import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
-@ApiStatus.OverrideOnly
 public interface LanguagePlayerHolder {
 
     @NotNull Language language();
--- src/main/java/ovh/roro/libraries/language/api/Translatable.java
+++ src/main/java/ovh/roro/libraries/language/api/Translatable.java
@@ -1,9 +1,7 @@
 package ovh.roro.libraries.language.api;
 
-import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
-@ApiStatus.OverrideOnly
 public interface Translatable {
 
     @NotNull String translationKey();
